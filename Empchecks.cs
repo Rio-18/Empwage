a003fa0c5c83553bacd9e5bd369387afc08b79c1
riya more <you@example.com>
2023-02-21 15:09:40 +0530

    calculate working hr reached for a month

--- Empchecks.cs
+++ Empchecks.cs
@@ -12,16 +12,20 @@ namespace Empwage
         public const int EMP_RATE_PER_HOUR = 20;
         public const int IS_PART_TIME = 2;
         public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 10;
 
 
         public static void CheckEmployee()
         {
             int empHrs = 0;
             int empWage = 0;
-            int totalEmpWage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
 
-            for (int day = 0; day < NUM_OF_WORKING_DAYS;day++)
+
+            while (totalEmpHrs<= MAX_HRS_IN_MONTH && totalWorkingDays< NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random1= new Random();
                 int empCheck= random1.Next(3);
 
@@ -38,19 +42,10 @@ namespace Empwage
                         break;
 
                 }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day # : "+ totalWorkingDays + "Emp Hrs : "+ empHrs);
             }
-           
-
-
-
-            
-
-            
-
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            totalEmpWage += empWage;
-            Console.WriteLine("Emp Wage : " + empWage);
-
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Emp Wage : " + totalEmpWage);
         }
             
