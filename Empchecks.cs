c69f15db7e345d489584832b128a78d2fcc1aa65
riya more <you@example.com>
2023-02-21 10:18:22 +0530

    Calculate Daily Empwage

---
+++ Empchecks.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empwage
+{
+    public class Empchecks
+    {
+        
+
+        public static void CheckEmployee()
+        {
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            int empcheck = random.Next(3);
+            
+            if(empcheck==IS_FULL_TIME) 
+            {
+                empHrs=8;
+
+            }
+            else
+            {
+                empHrs=0;
+            }
+            
+
+            
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+
+            Console.WriteLine("Emp Wage : " + empWage);
+        }
+            
+            
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Empwage
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Empwage Computation Program");
+            Empchecks.CheckEmployee();
+           
+
+            
+        }
+    }
+}
+
+
+
